d1081ce0c0cf5c8369a8c3c23bae21647ea661e0
Florian Bersier <info@xpressyoo.com>
2012-04-20 00:19:40 +0100

    new file:   README.md 	new file:   data/jquery.js 	new file:   data/script.js 	new file:   doc/main.md 	new file:   lib/main.js 	new file:   package.json 	new file:   prefchanges.xpi 	new file:   test/test-main.js

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the gmelius add-on.  It contains:
+
+* A program (lib/main.js).
+* A few tests.
+* Some meager documentation.
---
+++ data/script.js
@@ -0,0 +1,36 @@
+(function(){
+
+// Persistent Values
+self.port.on("get-prefs", function(prefs) {
+
+// onPrefchange Values
+self.port.on("prefchange", function(data) {	
+
+ 	$(document).ready(function() {
+
+  	var option1 	= prefs[0];
+  	var option2 	= prefs[1];
+  	var option 		= data[0];
+  	var value 		= data[1];
+ 	
+ 	var css;
+ 	
+		if(option1 == true || option == "option1" && value == true){
+			css = "body {background-color:red !important}";
+				}
+			
+  
+  		if(option2 == true || option == "option2" && value == true){
+			css = "body {background-color:blue !important}";
+
+ 		 		}
+ 		 		
+///////////////////////////////// CSS Activation
+$('head').append('<style type="text/css">' + css + '</style>');
+//////////////////////////////// #CSS Activation
+  
+	});//end jQuery ready
+ });
+ });
+
+})();
